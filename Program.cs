a6a595535f8b638448aa61db98a2fd03f38966da
Sisoni <cosplayers.help@gmail.com>
2026-02-03 13:39:24 +0200

    fixed spelling mistakes

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Random random = new Random();
+        int secretNumber = random.Next(1, 101); // число от 1 до 100
+        int guess = 0;
+        int attempts = 0;
+
+        Console.WriteLine("=== Guess The Number Game ===");
+        Console.WriteLine("I have chosen a number between 1 and 100.");
+
+        while (guess != secretNumber)
+        {
+            Console.Write("Enter your guess: ");
+            guess = int.Parse(Console.ReadLine());
+            attempts++;
+
+            if (guess < secretNumber)
+            {
+                Console.WriteLine("Too low!");
+            }
+            else if (guess > secretNumber)
+            {
+                Console.WriteLine("Too high!");
+            }
+            else
+            {
+                Console.WriteLine($"Correct! You guessed the number in {attempts} attempts.");
+            }
+        }
+
+        Console.WriteLine("Press any key to exit...");
+        Console.ReadKey();
+    }
+}
